39d5dc99ee30f8cb51fd1bcf63f444c4a41ffcaa
Mandy Torrence <mandolinhaynes@gmail.com>
2020-02-01 15:29:04 -0500

    Moving the server.js file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,11 +15,11 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "../../index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "../../notes.html"));
 });
 
 // Displays all notes
@@ -61,6 +61,9 @@ app.post("/api/notes", function (req, res) {
 
 // Starts the server to begin listening
 // =============================================================
+// app.listen(PORT, function () {
+//     console.log("App listening on PORT " + PORT);
+// });
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 });
